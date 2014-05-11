66ebe84478e25c5690bfd8a1c446f382dcab3f31
Vlado Driver <vladodriver@gmail.com>
2014-05-11 13:18:58 +0200

    Better detection old storage data, when update extension.

--- eventpage.js
+++ eventpage.js
@@ -14,21 +14,44 @@ var defopts = {
   disurls: ['www.google.cz']
 };
 
-var setonoff = function(host) {
-  chrome.storage.local.get(null, function(data) {
-    if (Object.keys(data).length === 0) {
-      setopts(defopts);
-    } else {
-      chrome.browserAction.setBadgeText({text: (data.disurls.indexOf(host) !== -1) ? 'OFF' : 'ON'});
-      chrome.browserAction.setBadgeBackgroundColor({color: (data.disurls.indexOf(host) !== -1) ? '#f00' : '#0f0'});
-    }
-  });
-};
-
 /*Save default options to storage when run first time*/
-var setopts = function(obj) {
-  chrome.storage.local.set(obj, function() {
-    console.log('TRS first time run -> default options loaded', obj);
+var setopts = function() {
+  chrome.storage.local.get(null, function(data) {
+    var obj = null;
+    if(Object.keys(data).length === 0) {
+      console.log('Storage is emty, full data (options && disurls) will be created!');
+      obj = defopts;
+    } else if (typeof data.options !== 'object' || Object.keys(data.options).length === 0) {
+      console.log('Valid object <options> not exists in storage, will be created from default options!');
+      obj = {};
+      obj.options = defopts.options;
+    } else if (typeof data.disurls !== 'object' || data.disurls.constructor.name !== 'Array') {
+      console.log('Valid object array <disurls> not exists in storage, will be created from default options!');
+      obj = {};
+      obj.disurls = defopts.disurls;
+    } else {
+      var invalidopt = false;
+      obj = obj || {options: {}}; //remporary crete object obj
+      for (var opt in defopts.options) {
+        /*Validation options format and data type*/
+        if(!data.options[opt] || typeof data.options[opt] !== 'object' || data.options[opt].constructor.name !== 'Array' || data.options[opt].length !== defopts.options[opt].length) {
+          console.log('Object property ' + opt + ' not a valid, will be set to default ' + defopts.options[opt] + '!');
+          obj.options[opt] = defopts.options[opt]; //restore from defaults
+          invalidopt = true;
+        } else {
+          obj.options[opt] = data.options[opt]; //keep user defined when is valid
+        }
+      }
+      if (!invalidopt) {
+        obj = null; //reset when all options is valid (invalidopt === false)
+      }
+    }
+    
+    if (obj) {
+      chrome.storage.local.set(obj, function() {
+        console.log('Default ptions loaded/restored', obj);
+      });
+    }
   });
 };
 
@@ -40,8 +63,19 @@ var tabaction = function(ev) {
     chrome.tabs.get(id, function(tab) {
       var h = document.createElement('a');
       h.href = tab.url;
+      console.log('ACTION', ev, 'URL', tab, 'ID', id);
       setonoff(h.host);
     });
+  });
+};
+
+var setonoff = function(host) {
+  setopts(host);
+  chrome.storage.local.get(null, function(data) {
+    if (data.disurls && data.disurls.constructor.name === 'Array') {
+      chrome.browserAction.setBadgeText({text: (data.disurls.indexOf(host) !== -1) ? 'OFF' : 'ON'});
+      chrome.browserAction.setBadgeBackgroundColor({color: (data.disurls.indexOf(host) !== -1) ? '#f00' : '#0f0'});
+    }
   });
 };
 
